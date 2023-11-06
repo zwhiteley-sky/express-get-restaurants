96e3597d254752f88e33790764d99c5a7bf76f49
Zachary Whiteley <zachary.whiteley@sky.uk>
2023-11-06 10:03:50 +0000

    Complete exercise

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-
-
-
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+});
 
 module.exports = app;
